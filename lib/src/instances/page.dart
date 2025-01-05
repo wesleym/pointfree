fe0edec394c1056fe2c9716dc959f0cf2ada9701
Wesley Moy <wesleym@lambdal.com>
2025-01-05 17:20:31 -0500

    Dump all info about instance to screen

--- lib/src/instances/page.dart
+++ lib/src/instances/page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:lambda_gui/src/instances/repository.dart';
 import 'package:lambda_gui/src/platform/scaffold.dart';
 import 'package:lambda_gui/src/platform/text_button.dart';
@@ -12,17 +13,39 @@ class InstancesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final instance = _instancesRepository.instances
+        .where((element) => element.id == instanceId)
+        .singleOrNull;
+    if (instance == null) {
+      _instancesRepository.update();
+      return CircularProgressIndicator.adaptive();
+    }
+
+    final platform = Theme.of(context).platform;
+    final Color color;
+    switch (platform) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        color = CupertinoColors.destructiveRed;
+      default:
+        color = Theme.of(context).colorScheme.error;
+    }
+
     return PlatformScaffold(
         topBar: PlatformTopBar(title: Text('GPU Instance')),
         body: ListView(children: [
-          Text(instanceId),
-          Row(children: [
+          Text(instance.name ?? instanceId),
+          Text('$instance'),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
             PlatformTextButton(
                 onPressed: () => _instancesRepository.restart(instanceId),
                 child: Text('Restart')),
             PlatformTextButton(
                 onPressed: () => _instancesRepository.terminate(instanceId),
-                child: Text('Terminate')),
+                child: Text(
+                  'Terminate',
+                  style: TextStyle(color: color),
+                )),
           ]),
         ]));
   }
