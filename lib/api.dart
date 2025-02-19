08cbce38cc4d761af1d989b38d70b2ad76d8fefd
Wesley Moy <wesleym@lambda.ai>
2025-02-19 17:31:32 -0500

    Update generated API
    This is version 1.6 of the Lambda Cloud external API.

--- lib/api.dart
+++ lib/api.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2025-01-03T13:42:05.469631
+// Openapi Generator last run: : 2025-02-19T17:32:35.531496
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 const specUrl = 'https://cloud.lambdalabs.com/api/v1/openapi.yaml';
