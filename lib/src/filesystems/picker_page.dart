19ab11d9b6c3bd165a90c5a299c2cac264aeb8f8
Wesley Moy <wesleym@lambda.ai>
2025-08-11 08:44:34 -0500

    Remove unused import

--- lib/src/filesystems/picker_page.dart
+++ lib/src/filesystems/picker_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lambda_gui/src/filesystems/repository.dart';
 import 'package:lambda_gui/src/instances/launch.dart';
