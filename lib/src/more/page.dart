88c52d1ae4c4897da0efebab3357bdd7d6b9ef03
Wesley Moy <wesleym@lambda.ai>
2025-08-12 06:57:04 -0500

    Add Clear API Key to Resources

--- lib/src/more/page.dart
+++ lib/src/more/page.dart
@@ -1,31 +1,83 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lambda_gui/src/login/store.dart';
 import 'package:lambda_gui/src/platform/list_tile.dart';
 import 'package:lambda_gui/src/platform/scaffold.dart';
+import 'package:lambda_gui/src/theme_type_provider.dart';
 
 class MorePage extends StatelessWidget {
   const MorePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var themeType = ThemeTypeProvider.of(context);
+
+    final list = switch (themeType) {
+      ThemeType.cupertino => ColoredBox(
+          color: CupertinoColors.systemGroupedBackground,
+          child: ListView(
+            children: [
+              CupertinoListSection(
+                hasLeading: false,
+                children: [
+                  PlatformListTile(
+                    title: Text('Storage'),
+                    onTap: () => context.go('/filesystems'),
+                  ),
+                  PlatformListTile(
+                    title: Text('SSH keys'),
+                    onTap: () => context.go('/ssh'),
+                  ),
+                  PlatformListTile(
+                    title: Text('Firewall'),
+                    onTap: () => context.go('/firewall'),
+                  ),
+                ],
+              ),
+              CupertinoListSection(
+                hasLeading: false,
+                children: [
+                  PlatformListTile(
+                    title: Text('Clear API key'),
+                    onTap: () => _clearApiKey(context),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ThemeType.material || ThemeType.lambda => ListView(
+          children: [
+            PlatformListTile(
+              title: Text('Storage'),
+              onTap: () => context.go('/filesystems'),
+            ),
+            PlatformListTile(
+              title: Text('SSH keys'),
+              onTap: () => context.go('/ssh'),
+            ),
+            PlatformListTile(
+              title: Text('Firewall'),
+              onTap: () => context.go('/firewall'),
+            ),
+            Divider(),
+            PlatformListTile(
+              title: Text('Clear API key'),
+              onTap: () => _clearApiKey(context),
+            ),
+          ],
+        ),
+    };
+
     return PlatformScaffold(
       topBar: PlatformTopBar(title: Text('Resources')),
-      body: ListView(
-        children: [
-          PlatformListTile(
-            title: Text('Storage'),
-            onTap: () => context.go('/filesystems'),
-          ),
-          PlatformListTile(
-            title: Text('SSH Keys'),
-            onTap: () => context.go('/ssh'),
-          ),
-          PlatformListTile(
-            title: Text('Firewall'),
-            onTap: () => context.go('/firewall'),
-          ),
-        ],
-      ),
+      body: list,
     );
+  }
+
+  Future<void> _clearApiKey(BuildContext context) async {
+    await LoginStore.instance.setApiKey(null);
+    if (context.mounted) context.go('/login');
   }
 }
